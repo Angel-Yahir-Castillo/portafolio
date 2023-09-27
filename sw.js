ce73d4697d55e36c6c853053524626fad206b0a9
Angel Yahir <yahirgamerpvz@gmail.com>
2023-09-27 00:02:00 -0600

    pwa

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ const CACHE_NAME = 'v1_cache_portafolio',
     'proyectos.html',
     'sobre-mi.html',
     'testimonios.html',
-    'imagenes'
+    'imagenes/*'
   ]
 
 //durante la fase de instalación, generalmente se almacena en caché los activos estáticos
